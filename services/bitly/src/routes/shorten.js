6be75a5d5b1a090ea8a192abf04eb5cfb8c2063b
Francisco Preller <francisco.preller@gmail.com>
2018-11-12 17:18:05 +1100

    Add multiple urls to the bitly API

--- services/bitly/src/routes/shorten.js
+++ services/bitly/src/routes/shorten.js
@@ -6,8 +6,22 @@ const router = express.Router();
 
 router.post('/', async (req, res) => {
   try {
-    const response = await Bitly.shortenUrl(req.body.url);
-    res.json(createResponse.success(response));
+    // If the parameter 'urls' is passed we take multiple
+    if (Object.keys(req.body).includes('urls') && Array.isArray(req.body.urls)) {
+      const response = await Promise.all(req.body.urls.map(async (url) => {
+        const shortUrl = await Bitly.shortenUrl(url);
+        return shortUrl.link;
+      }));
+      res.json(createResponse.success(response));
+    } else if (Object.keys(req.body).includes('url')) {
+      const response = await Bitly.shortenUrl(req.body.url);
+      res.json(createResponse.success(response));
+    } else {
+      res.status(400);
+      res.json(createResponse.error(
+        new Error('Request must contain fields \'url\' or \'urls\''),
+      ));
+    }
   } catch (error) {
     console.error('Error caught on shorten POST / route', error);
     res.status(error.status || 500);
